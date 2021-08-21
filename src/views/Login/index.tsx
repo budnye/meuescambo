3ae12631b168f0faac5475c0abe5725da72281d0
Eduardo Budny <51931984+budnye@users.noreply.github.com>
2021-08-21 12:12:23 -0300

    created LoginPage component

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Scroll } from './styles';
 
 import { SocialLogin } from './SocialLogin';
 import { LoginDivider } from './LoginDivider';
+import { LoginForm } from './LoginForm';
+import { LoginFooter } from './LoginFooter';
+import { KeyboardAvoidingView } from 'react-native';
+
 
 export function Login(){
   return(
-    <Container>
-      <SocialLogin />
-      <LoginDivider />
-      {/* <LoginForm /> */}
-    </Container>
+    <Scroll>
+      <KeyboardAvoidingView behavior="position">
+        <Container>
+        <SocialLogin />
+        <LoginDivider />
+        <LoginForm />
+        </Container>
+      </KeyboardAvoidingView>
+      <LoginFooter />
+    </Scroll >
+
   );
 };
--- src/views/Login/styles.ts
+++ src/views/Login/styles.ts
@@ -1,13 +1,17 @@
+import { RFPercentage } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex:1;
+  padding-top: ${RFPercentage(10)}px;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-
+export const Scroll = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  background-color: ${({ theme }) => theme.colors.background};
 `;
