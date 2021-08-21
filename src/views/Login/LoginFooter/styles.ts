751ddf5b3fb4ae0206ee9b264eed02c77230fd5c
Eduardo Budny <51931984+budnye@users.noreply.github.com>
2021-08-21 12:10:42 -0300

    created loginFooter component

---
+++ src/views/Login/LoginFooter/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize'
+export const Container = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background};
+  /* border: 1px solid red; */
+  padding: 8px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(10)}px;
+  text-align: center;
+  text-decoration: underline;
+  margin: ${RFValue(10)}px 0px;
+`;
