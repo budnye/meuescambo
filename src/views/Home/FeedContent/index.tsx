c776688493fcbbeab40429bd8fbf788a367188f9
Eduardo Budny <51931984+budnye@users.noreply.github.com>
2021-08-22 13:17:02 -0300

    created feedContent component

---
+++ src/views/Home/FeedContent/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function FeedContent(){
+  return(
+    <Container>
+      <Title>FeedContent</Title>
+    </Container>
+  );
+};
