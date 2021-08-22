092bfbf15c28854014c12babdf2d94abd7934a5e
Eduardo Budny <51931984+budnye@users.noreply.github.com>
2021-08-22 20:59:20 -0300

    adjusted style

--- src/views/Home/FeedContent/index.tsx
+++ src/views/Home/FeedContent/index.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 
 import { ListContainer, Title } from './styles';
+import { MainCard } from '../../../components/MainCard';
+
 
 export function FeedContent(){
   const data = [
     {
       id: 1,
-      name: 'John Doe',
+      name: 'Trailer para casal',
+      image_url: 'https://img.olx.com.br/images/30/306041802862002.jpg',
     },
     {
       id: 2,
-      name: 'Jane Doe',
+      name: 'Cama Solteiro',
+      image_url: 'https://img.olx.com.br/images/61/615168314735970.jpg',
     },
     {
       id: 3,
-      name: 'John Smith',
+      name: 'Relógio Diesel Top',
+      image_url: 'https://img.olx.com.br/images/17/177183662438250.jpg',
     },
     {
       id: 4,
-      name: 'Jane Smith',
+      name: 'Serviço de Carpintaria por hora',
+      image_url: 'https://www.pontorh.com.br/y/2725/carpinteiro-e1464631823154-720.jpg',
     },
   ]
   return(
     <ListContainer
           data={data}
           keyExtractor={item => item.id.toString()}
-          renderItem={({ item }) => <Title>{item.name}</Title>} 
+          renderItem={({ item }) => <MainCard title={item.name} image={item.image_url}/>} 
     />
   );
 };
--- src/views/Home/FeedContent/styles.ts
+++ src/views/Home/FeedContent/styles.ts
@@ -5,12 +5,15 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 export const ListContainer = styled(FlatList as new () => FlatList).attrs({
   showsVerticalScrollIndicator: false,
   contentContainerStyle:{
-    alignItems: 'center',
+    
+
   }
 })`
   margin-top: ${getStatusBarHeight()}px;
   width: 100%;
   padding: 8px 0px;
+  background-color: ${({ theme }) => theme.colors.background};
+
 `;
 
 export const Title = styled.Text`
