ff843e84bc5c770d3137291779c365b17acd0d0a
Eduardo Budny <51931984+budnye@users.noreply.github.com>
2021-08-22 20:59:59 -0300

    added card buttons

---
+++ src/components/CardButton/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+import { FontAwesome } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { ButtonProps } from '.'
+
+interface ContainerProps{
+  type?: 'small' | 'large';
+}
+
+export const Container = styled.View<ContainerProps>`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.colors.white};
+  width: ${({type}) => type == 'small' ? 40 : 45}px;
+  height: ${({type}) => type == 'small' ? 40 : 45}px;
+  border-radius: 50px;
+  margin: 8px 16px;
+  box-shadow: 3px 3px 3px #eee;
+`;
+
+export const Title = styled.Text`
+
+`;
+
+export const Icon = styled(FontAwesome)<ButtonProps>`
+  font-size: ${({ type }) => type == 'small' ? RFValue(16) : RFValue(18)}px;
+  margin-right: 8px;
+  margin-left: 8px;
+  color: ${({ theme, color }) => color ? color : theme.colors.primary};
+`;
+
