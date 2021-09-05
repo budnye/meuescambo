22f113b71b69d91d1a4cfe2ce6231ce11be49f8e
Eduardo Budny <budny.dev@gmail.com>
2021-09-05 19:02:49 -0300

    added label as a parameter to inputForm component

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -2,26 +2,30 @@ import React from 'react';
 import { Control, Controller } from 'react-hook-form';
 import { TextInputProps } from 'react-native';
 import { Input } from '../Input';
-import { Container, Error } from './styles';
+import { Container, Label, InputBox } from './styles';
 
 interface Props extends TextInputProps {
   control: Control;
   name: string;
   error: string;
+  label?: string;
 }
 
-export function InputForm({ control, name, error, ...rest }: Props) {
+export function InputForm({ control, name, error, label, ...rest }: Props) {
   return (
     <Container>
       <Controller
         control={control}
         render={({ field: { onChange, value } }) => (
+          <InputBox>
+          {label && <Label>{label}</Label>}
           <Input
             onChangeText={onChange}
             value={value}
             error={error}
             {...rest}
           />
+          </InputBox>
         )}
         name={name}
       ></Controller>
