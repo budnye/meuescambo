784a614168e6e0175acdfc9c2bc58b4d630e1e39
Eduardo Budny <51931984+budnye@users.noreply.github.com>
2021-09-23 16:50:31 -0300

    adjuste profile header

---
+++ src/views/Auth/Profile/ProfileOptions/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function ProfileOptions(){
+  return(
+    <Container>
+      <Title>ProfileOptions</Title>
+    </Container>
+  );
+};
