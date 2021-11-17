3dc187a67af78450f1f3c96621a9a84cb279c2ba
Eduardo Budny <51931984+budnye@users.noreply.github.com>
2021-11-17 11:14:00 -0300

    limit image size

--- src/views/Auth/Profile/index.tsx
+++ src/views/Auth/Profile/index.tsx
@@ -47,7 +47,9 @@ export function Profile({ navigation }) {
     let result: any = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: false,
-      quality: 1,
+      quality: 0.7,
+      maxWidth: 1024,
+      maxHeight: 1024,
     });
 
     console.log('RESULT IMAGE ROLL', result);
